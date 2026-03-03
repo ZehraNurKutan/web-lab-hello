c9c39e37520ea6e798b73d1ca058c747ac8a1e9c
ZEHRA NUR KUTAN <nurkutan34@gmail.com>
2026-03-03 13:04:08 +0300

    chore: initial project setup with Vite + React+ TS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+return (
+<div>
+<h1>Web Tasarimi ve Programlama </h1>
+ <h2>LAB -1</h2>
+ <p>Ad Soyad: [ZEHRA NUR KUTAN]</p>
+<p>Ogrenci No: [235541117]</p>
+</div>
+)
+export default App
