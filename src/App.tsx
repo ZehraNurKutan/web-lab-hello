738911ddb611f3b14fc38df85678c19182c4245b
Zehra Nur Kutan <nurkutan34@gmail.com>
2026-03-11 19:46:38 +0300

    feat: add personal details to homepage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama </h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: Zehra Nur KUTAN</p>
+      <p>Ogrenci No: 235541117</p>
+      <p>Bolum: Bilgisayar Programciligi</p>
+      <p>Hobiler: Kodlama, Kitap Okumak, Doga Yuruyusleri</p>
+      <p>Bu benim ilk React projem! Dunyaya merhaba!</p>
+    </div>
+  )
+}
+
+export default App
